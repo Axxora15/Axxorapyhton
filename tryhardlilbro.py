92ed29eb6af45fd939b063d1cd7c485592837731
Axxora15 <118996317+Axxora15@users.noreply.github.com>
2024-08-30 19:38:59 +0530

    Add files via upload

---
+++ tryhardlilbro.py
@@ -0,0 +1,19 @@
+a=input("Enter Your Name: ")
+b=input("Enter Your Opponents Name: ")
+d=print("Opponents turn")
+c=int(input("Enter a Number: "))
+print("Your Turn")
+print("You Have To Guess Weather ",b," Choose even or odd number.")
+e=int(input("IF U CHOOSE EVEN THEN TYPE 2 IF U CHOOSE ODD THEN TYPE 1: "))
+if(c%2==0):
+    f=2
+else:
+    f=1
+if(e==f):
+    print("U woon!")
+else:
+    print("Skill issue")
+    
+
+    
+   
